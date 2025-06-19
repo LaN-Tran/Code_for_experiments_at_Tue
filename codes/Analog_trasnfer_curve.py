a99145b302086bfc61d7267c410444e2361d4d30
Le Phuong Lan, Tran <l.p.l.tran@tue.nl>
2025-06-19 11:40:48 +0200

    modified:   codes/Analog_trasnfer_curve.py (not test)

--- codes/Analog_trasnfer_curve.py
+++ codes/Analog_trasnfer_curve.py
@@ -15,7 +15,8 @@ import time
 import sys
 from os import sep
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
+import csv
 
 if sys.platform.startswith("win"):
     dwf = cdll.dwf
@@ -31,6 +32,13 @@ sys.path.append(constants_path)
 import dwfconstants
 
 from dwfconstants import *
+
+import logging
+# init logger
+format = "%(asctime)s: %(message)s"
+log_file_path = 'example.log'
+logging.basicConfig(format=format, level=logging.INFO,  
+                        datefmt="%H:%M:%S", filename= log_file_path, filemode= 'w')
 
 version = create_string_buffer(16)
 dwf.FDwfGetVersion(version)
@@ -57,62 +65,186 @@ if hdwf.value == hdwfNone.value:
 dwf.FDwfDeviceAutoConfigureSet(hdwf, 0) # 0 = the device will only be configured when FDwf###Configure is called
 
 print("Configuring device...")
+# Resistor for current measurement (res)
+R = 0.00205806419e+3 # Ohm
+
+# wave gen
+w1_gate = 0
+w2_drain = 1
+
 # Drain: DC
-dwf.FDwfAnalogOutEnableSet(hdwf, 1, 1) 
-dwf.FDwfAnalogOutFunctionSet(hdwf, 1, funcDC)
-dwf.FDwfAnalogOutOffsetSet(hdwf, 1, c_double(-0.2))
-dwf.FDwfAnalogOutConfigure(hdwf, 1, 0)
+drain_voltage = -0.2 # [V]
+dwf.FDwfAnalogOutEnableSet(hdwf, c_int(w2_drain), 1) 
+dwf.FDwfAnalogOutFunctionSet(hdwf, c_int(w2_drain), funcDC)
+dwf.FDwfAnalogOutOffsetSet(hdwf, c_int(w2_drain), c_double(drain_voltage))
+# FDwfAnalogOutConfigure(HDWF hdwf, int idxChannel, int fStart)
+# fStart – Start the instrument: 0 stop, 1 start, 3 apply.
+dwf.FDwfAnalogOutConfigure(hdwf, c_int(w2_drain), 0)
 
-# Gate: -0.5V to 0.5V (in 5 steps) then 0.5 V back to -0.5V (in 5 steps), at 5Hz, 200ms total length
-dwf.FDwfAnalogOutEnableSet(hdwf, 0, 1) 
-dwf.FDwfAnalogOutFunctionSet(hdwf, 0, funcCustom) 
-dwf.FDwfAnalogOutFrequencySet(hdwf, 0, c_double(5))
-dwf.FDwfAnalogOutOffsetSet(hdwf, 0, c_double(0))
-dwf.FDwfAnalogOutAmplitudeSet(hdwf, 0, c_double(0.5))
-# values normalized to +-1 
-# ChatGPT: using Python's ctypes module to create a C-style array of 5 double values.
-rgSteps = (c_double*10)(-1.0, -0.6, -0.2, 0.2, 0.6, 1, 0.6, 0.2, -0.2, -0.6) 
-# The output value will be Offset + Sample*Amplitude
-# The Sample = values in rgSteps
-dwf.FDwfAnalogOutDataSet(hdwf, 0, rgSteps, len(rgSteps))
-dwf.FDwfAnalogOutRunSet(hdwf, 0, c_double(0.2))
-dwf.FDwfAnalogOutRepeatSet(hdwf, 0, 2)
-dwf.FDwfAnalogOutMasterSet(hdwf, 0, 1)
-dwf.FDwfAnalogOutConfigure(hdwf, 0, 0)
+gate_voltage = 0 # [V]
+dwf.FDwfAnalogOutEnableSet(hdwf, c_int(w1_gate), 1) 
+dwf.FDwfAnalogOutFunctionSet(hdwf, c_int(w1_gate), funcDC) 
+dwf.FDwfAnalogOutOffsetSet(hdwf, c_int(w1_gate), c_double(gate_voltage))
+dwf.FDwfAnalogOutConfigure(hdwf, c_int(w1_gate), 0)
 
-# scope: (50kHz*200ms x 2) samples at 50kHz, 200ms x 2
-dwf.FDwfAnalogInChannelEnableSet(hdwf, 0, 1)
-dwf.FDwfAnalogInChannelEnableSet(hdwf, 1, 1)
-dwf.FDwfAnalogInFrequencySet(hdwf, c_double(50e3))
-dwf.FDwfAnalogInChannelRangeSet(hdwf, 0, c_double(10.0))
-dwf.FDwfAnalogInChannelRangeSet(hdwf, 1, c_double(10.0))
-dwf.FDwfAnalogInBufferSizeSet(hdwf, 20000)
-dwf.FDwfAnalogInTriggerSourceSet(hdwf, trigsrcAnalogOut1) 
-dwf.FDwfAnalogInTriggerPositionSet(hdwf, c_double(0.01)) # 5ms, trigger at first sample
+# scope:
+buffer_size = 100
+rec_freq = 50e+3 
+# the collecting time: buffer_size/rec_freq
+range_set = 5
+osc_1_current = 0
+osc_2_volt = 1
+# FDwfAnalogInChannelEnableSet(HDWF hdwf, int idxChannel, int fEnable)
+# fEnable – Set TRUE to enable, FALSE to disable (default)
+dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(osc_1_current), 1)
+dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(osc_2_volt), 1)
+dwf.FDwfAnalogInFrequencySet(hdwf, c_double(rec_freq))
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(osc_1_current), c_double(range_set))
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(osc_2_volt), c_double(range_set))
+# FDwfAnalogInBufferSizeSet(HDWF hdwf, int nSize)
+dwf.FDwfAnalogInBufferSizeSet(hdwf, c_int(buffer_size))
+trgsrc = dwfconstants.trigsrcNone
+dwf.FDwfAnalogInTriggerSourceSet(hdwf, trgsrc) 
+# by default, ACQMODE: acqmodeSingle 
+# FDwfAnalogInConfigure(HDWF hdwf, int fReconfigure, int fStart)
 dwf.FDwfAnalogInConfigure(hdwf, 1, 0)
 
 print("Wait for the offset to stabilize...")
 time.sleep(1)
 
-print("Starting test...")
-dwf.FDwfAnalogInConfigure(hdwf, 0, 1)
-dwf.FDwfAnalogOutConfigure(hdwf, 1, 1)
+# prepare the sweep voltage
+file_path = "C:/Users/20245580/LabCode/Codes_For_Experiments/exp_data/20250617/transfer_curve.csv"
+        # Prepare record file
+field_names = ['time', 'i_channel', 'v_gate', 'i_gate']
+with open(file_path, 'w') as file:
+    file_writer = csv.DictWriter(file, fieldnames=field_names)
+    file_writer.writeheader()
 
-sts = c_int()
-while True:
-    dwf.FDwfAnalogInStatus(hdwf, 1, byref(sts))
-    if sts.value == DwfStateDone.value :
-        break
-    time.sleep(0.001)
-print("done")
+settle_time = 1 # [s]
+rest_duration = 1 # [s]
+number_of_measurements = 2
+gate_voltage_smallest = -0.5
+gate_voltage_largest = 0.5
+gate_voltage_step = 0.1
+voltage_list_forward = np.arange(gate_voltage_smallest, gate_voltage_largest, gate_voltage_step).tolist()
+voltage_list_backward = np.arange(gate_voltage_largest, gate_voltage_smallest, -gate_voltage_step).tolist()
 
-rgc1 = (c_double*20000)()
-rgc2 = (c_double*20000)()
-dwf.FDwfAnalogInStatusData(hdwf, 0, rgc1, len(rgc1)) # get channel 1 data
-dwf.FDwfAnalogInStatusData(hdwf, 1, rgc2, len(rgc2)) # get channel 2 data
 
+logging.info(f"starting the measurement process")
+    # start the instrument
+        # wavegen
+dwf.FDwfAnalogOutConfigure(hdwf, c_int(w1_gate), 1)
+dwf.FDwfAnalogOutConfigure(hdwf, c_int(w2_drain), 1)
+time.sleep(settle_time)
+    # start the measurement reference time
+start_time = time.time()
+            # start measurement
+for i in range(0, number_of_measurements):
+    # forward 
+    for idx, v in enumerate(voltage_list_forward):                     
+                # set the voltage (gate)
+        gate_voltage = v
+        logging.info(f"set gate voltage {gate_voltage=}")
+        dwf.FDwfAnalogOutOffsetSet(hdwf, c_int(w1_gate), c_double(gate_voltage))
+            # apply the reconfigure to wavegen
+        dwf.FDwfAnalogOutConfigure(hdwf, c_int(w1_gate), c_int(3))
+        time.sleep(settle_time)
+
+            # turn on both osc channels
+            # FDwfAnalogInConfigure(HDWF hdwf, int fReconfigure, int fStart)
+        dwf.FDwfAnalogInConfigure(hdwf, 0, 1)
+            # collect data with osc
+        sts = c_int()
+        while True:
+                # FDwfAnalogInStatus(HDWF hdwf, int fReadData, DwfState *psts)
+                # fReadData – TRUE if data should be read.
+                dwf.FDwfAnalogInStatus(hdwf, 1, byref(sts))
+                if sts.value == DwfStateDone.value :
+                    break
+                time.sleep(0.001)
+        print(f"done with {gate_voltage=}")
+
+        rgc1_current = (c_double*buffer_size)()
+        rgc2_volt = (c_double*buffer_size)()
+        dwf.FDwfAnalogInStatusData(hdwf, osc_1_current, rgc1_current, len(rgc1_current)) # get channel 1 data
+        dwf.FDwfAnalogInStatusData(hdwf, osc_2_volt, rgc2_volt, len(rgc2_volt)) # get channel 2 data
+
+        rgc1_current = list(rgc1_current)
+        rgc2_volt = list(rgc2_volt)
+
+        rgc1_current_avg = (sum(rgc1_current) / len(rgc1_current)) / R
+        rgc2_volt_avg = sum(rgc2_volt) / len(rgc2_volt)
+
+                # record to file
+        with open(file_path, 'a') as file: 
+                # NOTICE: THE WHILE LOOP/ FOR LOOP INSIDE -> NO CONSTANT UPDATE TO FILE AT ALL -> NO ANIMATION
+            file_writer = csv.DictWriter(file, fieldnames=field_names)
+        
+            info = {
+                        'time': time.time() - start_time,
+                        'i_channel': rgc1_current_avg,
+                        'v_gate': rgc2_volt_avg,
+                        'i_gate': rgc1_current_avg
+                                        }
+            logging.info(f"save {info=} to .csv")
+            file_writer.writerow(info)
+                                
+            # Rest between measurement
+        time.sleep(rest_duration)
+        
+        
+        # backward
+    for idx, v in enumerate(voltage_list_backward):     
+        # set the voltage (gate)
+        gate_voltage = v
+        logging.info(f"set gate voltage {gate_voltage=}")
+        dwf.FDwfAnalogOutOffsetSet(hdwf, c_int(w1_gate), c_double(gate_voltage))
+            # apply the reconfigure to wavegen
+        dwf.FDwfAnalogOutConfigure(hdwf, c_int(w1_gate), c_int(3))
+        time.sleep(settle_time)
+
+            # turn on both osc channels
+            # FDwfAnalogInConfigure(HDWF hdwf, int fReconfigure, int fStart)
+        dwf.FDwfAnalogInConfigure(hdwf, 0, 1)
+            # collect data with osc
+        sts = c_int()
+        while True:
+                # FDwfAnalogInStatus(HDWF hdwf, int fReadData, DwfState *psts)
+                # fReadData – TRUE if data should be read.
+                dwf.FDwfAnalogInStatus(hdwf, 1, byref(sts))
+                if sts.value == DwfStateDone.value :
+                    break
+                time.sleep(0.001)
+        print(f"done with {gate_voltage=}")
+
+        rgc1_current = (c_double*buffer_size)()
+        rgc2_volt = (c_double*buffer_size)()
+        dwf.FDwfAnalogInStatusData(hdwf, osc_1_current, rgc1_current, len(rgc1_current)) # get channel 1 data
+        dwf.FDwfAnalogInStatusData(hdwf, osc_2_volt, rgc2_volt, len(rgc2_volt)) # get channel 2 data
+
+        rgc1_current = list(rgc1_current)
+        rgc2_volt = list(rgc2_volt)
+
+        rgc1_current_avg = (sum(rgc1_current) / len(rgc1_current)) / R
+        rgc2_volt_avg = sum(rgc2_volt) / len(rgc2_volt)
+
+                # record to file
+        with open(file_path, 'a') as file: 
+                # NOTICE: THE WHILE LOOP/ FOR LOOP INSIDE -> NO CONSTANT UPDATE TO FILE AT ALL -> NO ANIMATION
+            file_writer = csv.DictWriter(file, fieldnames=field_names)
+        
+            info = {
+                        'time': time.time() - start_time,
+                        'i_channel': rgc1_current_avg,
+                        'v_gate': rgc2_volt_avg,
+                        'i_gate': rgc1_current_avg
+                                        }
+            logging.info(f"save {info=} to .csv")
+            file_writer.writerow(info)
+                                
+            # Rest between measurement
+        time.sleep(rest_duration)
+        
+print(f"All Done")
 dwf.FDwfDeviceCloseAll()
-
-plt.plot(numpy.fromiter(rgc2, dtype = numpy.float), numpy.fromiter(rgc1, dtype = numpy.float))
-plt.show()
 
