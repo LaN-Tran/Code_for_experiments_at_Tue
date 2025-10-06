06300eaa1ca21af8a1b109cb510739761a2386bd
Le Phuong Lan, Tran <l.p.l.tran@tue.nl>
2025-10-06 16:35:56 +0200

    deleted:    codes/STDP_automate_measurement.py
    new file:   codes/STDP_automate_parameter_prep.py
    - not finished
      - code to modify `codes/STDP_automate_process_record.py`
    
    new file:   codes/STDP_automate_process_record.py
    - not finished
      - change the tsp code for reading
      - check the record files
    
    new file:   codes/multiple_pulse_measurement_automate_stdp.tsp
    - not finished

---
+++ codes/STDP_automate_parameter_prep.py
@@ -0,0 +1,242 @@
+"""
+    Automation Keithley 2602B, Arduino
+    STDP
+    Author:  Tran Le Phuong Lan.
+    Created:  2025-10-06
+
+    Requires:                       
+        Python 2.7, 3
+        pyfirmata
+        keithley_transfer_curve_stdp_automate.py
+        keithley_interchannelPulseTrain_dg_stdp_automate.py
+            pulse_train_2ch_dg_automate_stdp.py
+        keithley_interchannelPulseTrain_gd_stdp_automate.py
+            pulse_train_2ch_gd_automate_stdp.py
+        single_pulse_measurement_automate_stdp.py
+
+    Reference:
+
+"""
+import time
+import logging
+import pyfirmata
+import csv
+from datetime import datetime
+
+    # # ======
+    # # Logger
+    # # ======
+# init logger
+format = "%(asctime)s: %(message)s"
+log_file_path = 'example.log'
+logging.basicConfig(format=format, level=logging.INFO,  
+                        datefmt="%H:%M:%S", filename= log_file_path, filemode= 'w')
+
+
+#         # # ======
+#         # # Measurement setup
+#         # # ======
+logging.info("MEASUREMENT SETUP")
+
+logging.info("MODIFY keithley_transfer_curve_stdp_automate.py")
+    # Read file and modify content
+filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\keithley_transfer_curve_automate_stdp.py'
+with open(filename, 'r') as file:
+    lines = file.readlines()
+
+    # Modify the path for record file
+print(f"{'='*5}\nTransfer curve file content modification\n{'='*5}")
+print(f"!! Modify path of the recorded file")
+exp_date = input("enter <date> for folder exp_data/<date>, NO SPACE: ")
+lines[56]= 'file_path = \"C:/Users/20245580/LabCode/Codes_For_Experiments/exp_data' \
+                        + '/' + exp_date + '/transfer_curve.csv\"' \
+                        + '\n' 
+
+# Write the modified lines back to the file
+with open(filename, 'w') as file:
+    file.writelines(lines)
+
+logging.info("MODIFY single_pulse_measurement_automate_stdp.tsp")
+print(f"{'='*5}\nsingle_pulse_measurement_automate_stdp.tsp\n{'='*5}")
+filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\single_pulse_measurement_automate_stdp.tsp'
+with open(filename, 'r') as file:
+    lines = file.readlines()
+
+print(f"!! read pulse amplitude, pulse width, pulse period,...")
+read_pulse_amp = input("enter \\read pulse amplitude [V]\\, NO SPACE: ")
+t_on = input("enter \\read pulse width [s]\\, NO SPACE: ")
+t_off = input("enter \\read pulse off [s]\\, NO SPACE: ")
+number_of_rpulses = input("enter \\number of read pulses\\, NO SPACE: ")
+    # level
+lines[14]= 'level = ' + read_pulse_amp\
+            + '\n' 
+    # ton
+lines[16]= 'ton = ' + t_on\
+            + '\n' 
+    # toff
+lines[18]= 'toff = ' + t_off\
+            + '\n' 
+    # # of rpulses
+lines[20]= 'points = ' + number_of_rpulses\
+            + '\n' 
+
+# Write the modified lines back to the file
+with open(filename, 'w') as file:
+    file.writelines(lines)
+
+logging.info("FINISH MODIFY single_pulse_measurement_automate_stdp.tsp")
+
+logging.info("MODIFY pulse_train_2ch_dg_automate_stdp.tsp")
+    # Read file and modify content
+print(f"{'='*5}\npulse_train_2ch_dg_automate_stdp.tsp\n{'='*5}")
+filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\pulse_train_2ch_dg_automate_stdp.tsp'
+with open(filename, 'r') as file:
+    lines = file.readlines()
+
+print(f"!! Pulse amplitude, pulse width, pulse period, delta_t")
+Vd_amp = input("enter \\Vd amplitude [V]\\, NO SPACE: ")
+Vg_amp = input("enter \\Vg amplitude [V]\\, NO SPACE: ")
+pulse_period = input("enter \\Pulse Period [s]\\, NO SPACE: ")
+pulse_width = input("enter \\Pulse Width [s]\\, NO SPACE: ")
+delta_t = input("enter \\Delta_t [V]\\, NO SPACE: ")
+number_of_pulses = input("enter \\Number of Pulses -1\\, NO SPACE: ")
+    # Vd
+lines[18]= 'pulse_vd = ' + Vd_amp\
+            + '\n' 
+    # Vg
+lines[19]= 'pulse_vg = ' + Vg_amp\
+            + '\n' 
+    # pulse period
+lines[39]= 'pulse_period = ' + pulse_period\
+            + '\n' 
+    # pulse width
+lines[40]= 'pulse_width = ' + pulse_width\
+            + '\n' 
+    # delta_t
+lines[41]= 'deta_t = ' + delta_t\
+            + '\n' 
+    # number of pulses
+lines[44]= 'number_pulses= ' + number_of_pulses\
+            + '\n' 
+
+# Write the modified lines back to the file
+with open(filename, 'w') as file:
+    file.writelines(lines)
+logging.info("FINISH MODIFY pulse_train_2ch_dg_automate_stdp.tsp")
+
+logging.info("MODIFY pulse_train_2ch_gd_automate_stdp.tsp")
+    # Read file and modify content
+print(f"{'='*5}\npulse_train_2ch_gd_automate_stdp.tsp\n{'='*5}")
+print(f"Same parameters with pulse_train_2ch_dg_automate_stdp.tsp")
+filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\pulse_train_2ch_gd_automate_stdp.tsp'
+with open(filename, 'r') as file:
+    lines = file.readlines()
+
+    # Vd
+lines[18]= 'pulse_vd = ' + Vd_amp\
+            + '\n' 
+    # Vg
+lines[19]= 'pulse_vg = ' + Vg_amp\
+            + '\n' 
+    # pulse period
+lines[39]= 'pulse_period = ' + pulse_period\
+            + '\n' 
+    # pulse width
+lines[40]= 'pulse_width = ' + pulse_width\
+            + '\n' 
+    # delta_t
+lines[41]= 'deta_t = ' + delta_t\
+            + '\n' 
+    # number of pulses
+lines[44]= 'number_pulses= ' + number_of_pulses\
+            + '\n' 
+
+# Write the modified lines back to the file
+with open(filename, 'w') as file:
+    file.writelines(lines)
+logging.info("FINISH MODIFY pulse_train_2ch_gd_automate_stdp.tsp")
+
+logging.info("MODIFY keithley_interchannelPulseTrain_stdp_automate.py")
+    # Read file and modify content
+print(f"{'='*5}\keithley_interchannelPulseTrain_stdp_automate.py\n{'='*5}")
+filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\keithley_interchannelPulseTrain_stdp_automate.py'
+with open(filename, 'r') as file:
+    lines = file.readlines()
+
+nexp = input("enter \\number of exps\\, NO SPACE: ")
+wait_read_to_write = input("enter \\wait r-t-w [s]\\, NO SPACE: ")
+wait_write_to_read = input("enter \\wait w-t-r [s]\\, NO SPACE: ")
+
+    # Vd
+lines[65]= 'vd_amplitude = ' + Vd_amp + ' # [V]'\
+            + '\n' 
+    # Vg
+lines[66]= 'vg_amp = ' + Vg_amp + ' # [V]'\
+            + '\n' 
+    # w pulse period
+lines[70]= 'pulse_period = ' + pulse_period + ' # [s]'\
+            + '\n' 
+    # w pulse width
+lines[71]= 'pulse_width = ' + pulse_width + ' # [s]'\
+            + '\n' 
+    # delta_t
+lines[72]= 'delta_tpre_tpost = ' + delta_t + ' # [s]'\
+            + '\n' 
+    # number of w pulses
+nwp = str(int(number_of_pulses) + 1)
+lines[73]= 'n_write_cycle = ' + nwp\
+            + '\n' 
+    # r pulse width
+lines[80]= 'pulse_width_read = ' + t_on + ' # [s]'\
+            + '\n' 
+    # r pulse off
+lines[81]= 'read_pulse_off = ' + t_off + ' # [s]'\
+            + '\n' 
+    # # of r pulses
+lines[82]= 'number_read_pulses = ' + number_of_rpulses \
+            + '\n' 
+    # record file exp path
+lines[89]= 'file_path = \"C:/Users/20245580/LabCode/Codes_For_Experiments/exp_data' \
+                        + '/' + exp_date + '/pulse_exp.csv\"' \
+                        + '\n' 
+    # record file exp_avg path
+lines[90]= 'file_path_avg = \"C:/Users/20245580/LabCode/Codes_For_Experiments/exp_data' \
+                        + '/' + exp_date + '/pulse_exp_avg.csv\"' \
+                        + '\n' 
+    # number of exps
+lines[126]= '    nexp = ' + nexp\
+            + '\n' 
+    # wait r-t-w
+lines[128]= '    wait_between_read_and_write = ' + wait_read_to_write + ' # [s]'\
+            + '\n' 
+    # wait r-t-w
+lines[129]= '    wait_between_exp = ' + wait_write_to_read \
+            + ' # [s] = wait between write and read'\
+            + '\n' 
+
+# Write the modified lines back to the file
+with open(filename, 'w') as file:
+    file.writelines(lines)
+logging.info("FINISH MODIFY keithley_interchannelPulseTrain_stdp_automate.py")
+
+
+logging.info("Set dg (Drain=Pre before Gate=Post)")
+filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\keithley_interchannelPulseTrain_stdp_automate.py'
+with open(filename, 'r') as file:
+    lines = file.readlines()
+    # point to tsp run file
+lines[51]= 'file_tsp_path = \"C:/Users/20245580/LabCode/Codes_For_Experiments/codes/pulse_train_2ch_dg_automate_stdp.tsp\"' \
+                        + '\n' 
+    # point to tsp read file
+lines[58]= 'file_tsp_path = \"C:/Users/20245580/LabCode/Codes_For_Experiments/codes/single_pulse_measurement_automate_stdp.tsp\"' \
+                        + '\n' 
+# Write the modified lines back to the file
+with open(filename, 'w') as file:
+    file.writelines(lines)
+
+            # record the voltage pulse (amplitude, pulse width, pulse periode, # of pulses) + delta (t_pre-t_post)
+        # run code `?` to only measured the g_after.. in a duration of 20min -> collect an array of g_after
+        # process the array of g_after to array of percentage of change (g_after's - g_init)/ g_init
+            # record this array of g_after's + time 
+        # process average (of array percentage of change) 
+            # record average (of array percentage of change) + delta -> a fully stdp file
---
+++ codes/STDP_automate_process_record.py
@@ -0,0 +1,357 @@
+"""
+    Automation Keithley 2602B, AD3
+    Keithley pulse drain (smua), pulse gate (smub)
+    Author:  Tran Le Phuong Lan.
+    Created:  2025-09-05
+
+    Requires:                       
+       Python 2.7, 3
+       pyvisa
+       pyusb
+       Keithley2600
+    Reference:
+        [1] https://github.com/LaN-Tran/Automate_Lab_Instrument/tree/main/20250905
+
+"""
+
+import pyvisa
+import time
+import logging
+import pyfirmata
+import csv
+import os
+import sys
+from datetime import datetime
+
+
+    # # ======
+    # # Logger
+    # # ======
+# init logger
+format = "%(asctime)s: %(message)s"
+log_file_path = 'example.log'
+logging.basicConfig(format=format, level=logging.INFO,  
+                        datefmt="%H:%M:%S", filename= log_file_path, filemode= 'w')
+
+        # # ======
+        # # Arduino board, change between read and write
+        # # ======
+logging.info("configure arduino mux")
+    # MUX 1 (Y0: transfer curve, Y1: ecram)
+# arduino_bin_mux1_z =  3# 
+
+# arduino_bin_mux1_s0 = 5 # (lsb)
+# arduino_bin_mux1_s1 = 7 #
+# arduino_bin_mux1_s2 = 9 # (msb)
+
+# arduino_bin_mux1_enable = 11 #
+
+#     # MUX 2 (Y0: transfer curve, Y1: ecram)
+# arduino_bin_mux2_z = 2 # (HIGH = OFF/ LOW = ON)
+
+# arduino_bin_mux2_s0 = 4 # (lsb)
+# arduino_bin_mux2_s1 = 6 #
+# arduino_bin_mux2_s2 = 8 # (msb)
+
+# arduino_bin_mux2_enable = 10 #
+#     # init arduino aboard
+# arduino_board = pyfirmata.Arduino('COM8')
+
+        # ======
+        # Keithley, smua drain for read
+        # ======
+rm = pyvisa.ResourceManager('C:/windows/System32/visa64.dll')
+logging.info("OPEN keithley resource for reading")
+keithley_instrument = rm.open_resource('TCPIP0::169.254.0.1::inst0::INSTR')
+keithley_instrument.timeout = 10000
+
+
+        # ======
+        # Upload the keithley scripts to keithley for the program
+        # ======
+
+# script for reading phase
+file_tsp_path = "C:/Users/20245580/LabCode/Codes_For_Experiments/codes/single_pulse_measurement_automate_stdp.tsp"
+logging.info("UPLOAD read keithley ")
+keithley_instrument.write(f"loadscript Read")
+with open(file_tsp_path) as fp:
+    for line in fp: keithley_instrument.write(line)
+keithley_instrument.write("endscript") 
+
+        # # ======
+        # # record to file
+        # # ======
+file_path = "C:/Users/20245580/LabCode/Codes_For_Experiments/exp_data/20250980/stdp_process_monitor_exp.csv"
+file_path_processed_stdp= "C:/Users/20245580/LabCode/Codes_For_Experiments/exp_data/20250980/stdp_processed.csv"
+                # ======
+                # Prepare record file
+                # ======
+logging.info("Prepare record file")
+field_names = ['time', 'i_channel', 'date_time', 'comment']
+if os.path.exists(file_path):
+        print("File exists.")
+else:
+        print("File not exist. create file")
+        with open(file_path, 'a') as file:
+                file_writer = csv.DictWriter(file, fieldnames=field_names)
+                file_writer.writeheader()
+
+field_names_processed = ['delta', 'change_percentage']
+if os.path.exists(file_path_processed_stdp):
+        print("File exists.")
+else:
+        print("File not exist. create file")
+        with open(file_path_processed_stdp, 'a') as file:
+                file_writer = csv.DictWriter(file, fieldnames=field_names_processed)
+                file_writer.writeheader()
+
+        # ======
+        # Measurement parameters
+        # ======
+pulse_width_read = 2 # [s]
+read_pulse_off = 3 # [s]
+number_read_pulses = 4
+pulse_period_read = pulse_width_read +  read_pulse_off # [s]
+read_func_complete = (pulse_period_read)*number_read_pulses
+
+        # ======
+        # start measurement
+        # ======
+logging.info("start measurement")
+time_ref = time.time()
+
+logging.info("comment about the exp")
+comment_exp = input("comment about exp (dg or gd): ")
+
+try:
+    
+        logging.info("CONFIGURE transfer curve setup")
+        # # arduino mux1 connect to Y0
+                # Y0 configure
+        # arduino_board.digital[arduino_bin_mux1_s0].write(0)
+        # arduino_board.digital[arduino_bin_mux1_s1].write(0)
+        # arduino_board.digital[arduino_bin_mux1_s2].write(0)
+        #         # close switch
+        # arduino_board.digital[arduino_bin_mux1_enable].write(0)
+        # time.sleep(arduino_settle_time)
+
+        # # # arduino mux2 connect to Y0
+        #         # Y0 configure
+        # arduino_board.digital[arduino_bin_mux2_s0].write(0)
+        # arduino_board.digital[arduino_bin_mux2_s1].write(0)
+        # arduino_board.digital[arduino_bin_mux2_s2].write(0)
+        #         # close switch
+        # arduino_board.digital[arduino_bin_mux2_enable].write(0)
+        # time.sleep(arduino_settle_time)
+        logging.info("FINISH transfer curve setup")
+        
+        logging.info("RUN transfer curve")
+        # import keithley_transfer_curve_automate_stdp.py
+                #         # open switch
+        # arduino_board.digital[arduino_bin_mux1_enable].write(1)
+        # arduino_board.digital[arduino_bin_mux2_enable].write(1)
+        logging.info("FINISH RUN transfer curve")
+
+        logging.info("CONFIGURE ECRAM setup")
+        # # arduino mux1 connect to Y1
+                # Y1 configure
+        # arduino_board.digital[arduino_bin_mux1_s0].write(0)
+        # arduino_board.digital[arduino_bin_mux1_s1].write(1)
+        # arduino_board.digital[arduino_bin_mux1_s2].write(0)
+        #         # close switch
+        # arduino_board.digital[arduino_bin_mux1_enable].write(0)
+        # time.sleep(arduino_settle_time)
+
+        # # # arduino mux2 connect to Y1
+        #         # Y1 configure
+        # arduino_board.digital[arduino_bin_mux2_s0].write(0)
+        # arduino_board.digital[arduino_bin_mux2_s1].write(1)
+        # arduino_board.digital[arduino_bin_mux2_s2].write(0)
+        #         # close switch
+        # arduino_board.digital[arduino_bin_mux2_enable].write(0)
+        # time.sleep(arduino_settle_time)
+        logging.info("FINISH ECRAM setup")
+
+
+        logging.info("STDP MEASUREMENT Start")
+
+        # REFERENCE TIME
+        time_ref = time.time()
+                        # read
+        logging.info("reading initial channel conductance")
+        print("read phase")
+        record_g_init = []
+        for idx_exp in range(0, num_rps):
+
+                # time.sleep(wait_between_read_and_write)
+                logging.info("writing phase")
+                print("writing phase")
+                keithley_instrument.write("Write.run()") 
+
+                time.sleep(write_func_complete)
+                
+                logging.info("writing successfully")
+
+                time.sleep(wait_between_read_and_write)
+                
+                # read
+                logging.info("reading phase")
+                print("read phase")
+                try:
+                        keithley_instrument.write("Read.run()")
+
+                        time.sleep(read_func_complete)
+
+                        logging.info(f"read successfully")
+                        # save to file
+                        cur_time = time.time()
+                        cur_datetime = datetime.now()
+                        n_samples = int(float(keithley_instrument.query(f"print(smua.nvbuffer1.n)")))
+                        average = 0
+                        for i in range(0, n_samples):
+                                measured_i = float(keithley_instrument.query(f"print(smua.nvbuffer1.readings[{i}+1])"))
+                                keithely_time_stamp = float(keithley_instrument.query(f"print(smua.nvbuffer1.timestamps[{i}+1])"))
+                                measured_vd = float(keithley_instrument.query(f"print(smua.nvbuffer1.sourcevalues[{i}+1])"))
+                                with open(file_path, 'a') as file: 
+                                                # NOTICE: THE WHILE LOOP/ FOR LOOP INSIDE -> NO CONSTANT UPDATE TO FILE AT ALL -> NO ANIMATION
+                                        file_writer = csv.DictWriter(file, fieldnames=field_names)
+                                        info = {
+                                                'time':cur_time - time_ref + keithely_time_stamp,
+                                                'i_channel': measured_i,
+                                                'date_time': cur_datetime,
+                                                'comment': comment_exp + '; vg: [V]' + str(vg_amp) 
+                                                                + '; vd: [V]'+ str(vd_amplitude) 
+                                                                + '; read_pulse: [V]'+ str(measured_vd)
+                                                                + '; rpulse_width [s]: ' + str(pulse_width_read)
+                                                                + '; rpulse_period [s]: ' + str(pulse_period_read)
+                                                                + '; delta_t: [s]' + str(delta_tpre_tpost)
+                                                                + '; pulse_width [s]: ' + str(pulse_width)
+                                                                + '; pulse_period [s]: ' + str(pulse_period)
+                                                                + '; n_read_points: ' + str(n_samples)
+                                                                + '; n_write_cycle : ' + str(n_write_cycle)
+                                                                + '; sw_settle_time [s]: ' + str(sw_settle_time)
+                                                                + '; wait_between_read_and_write [s]: ' + str(wait_between_read_and_write)
+                                                                + '; wait_between_exp [s]: ' + str(wait_between_exp),
+
+                                                }
+                                        file_writer.writerow(info)
+                                average = average + measured_i
+                        average = average/ n_samples
+                        record_g_init.append(average)
+                except Exception as e:
+                        logging.info(f"Keithley error: {e=}")
+                        logging.info(f"EXIT: {e=}")
+                        sys.exit(-1)
+
+                # wait between exp
+                time.sleep(wait_between_exp)
+        
+        logging.info("FINISH initial read, close keithley resource for another code")
+        rm.close()
+        
+        logging.info("Set dg/gd (Drain=Pre before Gate=Post)")
+        filename = 'C:\\Users\\20245580\\LabCode\\Codes_For_Experiments\\codes\\keithley_interchannelPulseTrain_stdp_automate.py'
+        with open(filename, 'r') as file:
+                lines = file.readlines()
+        # point to tsp run file
+        lines[51]= 'file_tsp_path = \"C:/Users/20245580/LabCode/Codes_For_Experiments/codes/pulse_train_2ch_dg_automate_stdp.tsp\"' \
+                                + '\n' 
+        # Write the modified lines back to the file
+        with open(filename, 'w') as file:
+                file.writelines(lines)
+        
+        # record the delta_t
+        delta_t = -10
+
+        logging.info("RUN dg/gd (Drain=Pre before Gate=Post)")
+        # import keithley_transfer_curve_automate_stdp.py
+        logging.info("FINISH dg (Drain=Pre before Gate=Post)")
+        
+        logging.info("READING channel conductance, after stdp")
+
+        logging.info("OPEN keithley resource for reading")
+        keithley_instrument = rm.open_resource('TCPIP0::169.254.0.1::inst0::INSTR')
+        keithley_instrument.timeout = 10000
+        logging.info("UPLOAD read keithley ")
+        keithley_instrument.write(f"loadscript Read")
+        with open(file_tsp_path) as fp:
+                for line in fp: keithley_instrument.write(line)
+        keithley_instrument.write("endscript") 
+
+        logging.info("READING... ")
+        record_g_after = []
+        for idx_exp in range(0, num_rps_after_stdp):
+                
+                # read
+                logging.info("reading phase")
+                print("read phase")
+                try:
+                        keithley_instrument.write("Read.run()")
+
+                        time.sleep(read_func_complete)
+
+                        logging.info(f"read successfully")
+                        # save to file
+                        cur_time = time.time()
+                        cur_datetime = datetime.now()
+                        n_samples = int(float(keithley_instrument.query(f"print(smua.nvbuffer1.n)")))
+                        average = 0
+                        for i in range(0, n_samples):
+                                measured_i = float(keithley_instrument.query(f"print(smua.nvbuffer1.readings[{i}+1])"))
+                                keithely_time_stamp = float(keithley_instrument.query(f"print(smua.nvbuffer1.timestamps[{i}+1])"))
+                                measured_vd = float(keithley_instrument.query(f"print(smua.nvbuffer1.sourcevalues[{i}+1])"))
+                                with open(file_path, 'a') as file: 
+                                                # NOTICE: THE WHILE LOOP/ FOR LOOP INSIDE -> NO CONSTANT UPDATE TO FILE AT ALL -> NO ANIMATION
+                                        file_writer = csv.DictWriter(file, fieldnames=field_names)
+                                        info = {
+                                                'time':cur_time - time_ref + keithely_time_stamp,
+                                                'i_channel': measured_i,
+                                                'date_time': cur_datetime,
+                                                'comment': comment_exp + '; vg: [V]' + str(vg_amp) 
+                                                                + '; vd: [V]'+ str(vd_amplitude) 
+                                                                + '; read_pulse: [V]'+ str(measured_vd)
+                                                                + '; rpulse_width [s]: ' + str(pulse_width_read)
+                                                                + '; rpulse_period [s]: ' + str(pulse_period_read)
+                                                                + '; delta_t: [s]' + str(delta_tpre_tpost)
+                                                                + '; pulse_width [s]: ' + str(pulse_width)
+                                                                + '; pulse_period [s]: ' + str(pulse_period)
+                                                                + '; n_read_points: ' + str(n_samples)
+                                                                + '; n_write_cycle : ' + str(n_write_cycle)
+                                                                + '; sw_settle_time [s]: ' + str(sw_settle_time)
+                                                                + '; wait_between_read_and_write [s]: ' + str(wait_between_read_and_write)
+                                                                + '; wait_between_exp [s]: ' + str(wait_between_exp),
+
+                                                }
+                                        file_writer.writerow(info)
+                                average = average + measured_i
+                        average = average/ n_samples
+                        record_g_after.append(average)
+                except Exception as e:
+                        logging.info(f"Keithley error: {e=}")
+                        logging.info(f"EXIT: {e=}")
+                        sys.exit(-1)
+
+                # wait between exp
+                time.sleep(wait_between_exp)
+
+        logging.info("MEASUREMENT FINISH")
+        
+        logging.info("POST STDP PROCESSING")
+        g_init_avg = sum(record_g_init)/len(record_g_init)
+        g_after_avg = sum(record_g_after)/len(record_g_after)
+        percentage_change =  (g_after_avg - g_init_avg)/ g_init_avg
+        with open(file_path_processed_stdp, 'a') as file: 
+                # NOTICE: THE WHILE LOOP/ FOR LOOP INSIDE -> NO CONSTANT UPDATE TO FILE AT ALL -> NO ANIMATION
+                file_writer = csv.DictWriter(file, fieldnames=field_names_processed)
+                info = {
+                                                'delta': delta_t,
+                                                'change_percentage': percentage_change
+                                                }
+                file_writer.writerow(info)
+
+except KeyboardInterrupt:
+    print(f"MEASUREMENT: EXIT KeyboardInterrupt")
+    keithley_instrument.write(f"smua.source.output = smua.OUTPUT_OFF")
+    keithley_instrument.write(f"smub.source.output = smub.OUTPUT_OFF")
+    rm.close()
+
